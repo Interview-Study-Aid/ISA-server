2a37e04c0967cb904a4536339e0b8c75a2ba967a
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-11 20:07:30 -0700

    added user dal

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,23 @@
 const express = require('express');
 const router = express.Router();
 const getAllQuestions = require('../util/qs.js');
+const {createUser} = require('../util/user.js')
 
 router.get('/categories', async (req, res, next) => {
     let info = await getAllQuestions();
     res.status(200).json(info);
 })
+
+router.post('/signup', async (req, res, next) => {
+    console.log(req.body)
+    let user = await createUser(req.body.userName, req.body.userPassword);
+    res.status(200).json(user);
+})
+
+// router.get('/login', async (req, res, next) => {
+//     let info = await getAllQuestions();
+//     res.status(200).json(info);
+// })
 
 
 
---
+++ util/user.js
@@ -0,0 +1,52 @@
+var AWS = require("aws-sdk");
+var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+AWS.config.credentials = credentials;
+AWS.config.update({region: "us-east-1"});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+const handleCreate = (userName, userPassword) => {
+    var params = {
+        TableName: "UserTable",
+        Item:{
+            "userName": userName,
+            "userPassword": userPassword
+        }
+    };
+
+    console.log("Adding a new item...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+
+    return {userName, userPassword};
+}
+
+const handleGet = (userName) => {
+    var params = {
+        TableName: "UserTable",
+        Key:{
+            "userName": userName
+        }
+    };
+
+    docClient.get(params, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            return data;
+        }
+    });
+}
+
+
+
+
+module.exports = {
+    createUser:handleCreate,
+    handleGet:handleGet
+}
